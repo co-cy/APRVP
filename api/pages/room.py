94a280406c3e0db583264c1c5782d490521470cf
_co_cy_ <stf2003.kem@gmail.com>
2021-09-18 09:02:30 +0300

    decorate code
    Signed-off-by: _co_cy_ <stf2003.kem@gmail.com>

--- api/pages/room.py
+++ api/pages/room.py
@@ -1,6 +1,7 @@
 from api.database.tables.room import Room as TableRoom
 from flask_restful import Resource, reqparse
-import Math
+import math
+
 
 class Room(Resource):
     def get(self, request_type: str):
@@ -21,18 +22,15 @@ class Room(Resource):
             parser.add_argument('smoking', type=bool, required=True)  # другие курят +1, не курят -1
 
             # neighbors ЧТО 100 % должно быть в комнате
-            parser.add_argument('neighborsAge', type=list, required=True)
+            parser.add_argument('neighborsAge', type=int, action='append', required=True)
             parser.add_argument('neighborsHasPet', type=bool, required=True)
             parser.add_argument('neighborsSmoking', type=bool, required=True)
             parser.add_argument('neighborsHasChild', type=bool, required=True)
-            
 
             # ОТСЮДа брать данные о пользователе по типу all_user_parameters["gender"] или all_user_parameters["age"]
             all_user_parameters = parser.parse_args()
-
             alternative_list = []
             good_list = []
-
 
             # ЗАДАЧА: Посчитать сколько баллов получает комната
             for room in TableRoom.query.filter_by(is_free=True).all():
@@ -47,7 +45,7 @@ class Room(Resource):
                 isAlternative = False
                 GraftCount = 0
                 room_points = 0
-                for passenger in all_passengers: 
+                for passenger in all_passengers:
                     passenger_points = 0
                     if passenger.hasPet != all_user_parameters["neighborsHasPet"]:
                         isAlternative = True
@@ -77,21 +75,22 @@ class Room(Resource):
                             difference = min_neighborsAge - passenger.age
                         passenger_points += pointsForAge(difference)
 
-                    room_points += passenger_points/max_count_passenger
-                
+                    room_points += passenger_points / max_count_passenger
+
                 if GraftCount >= len(all_passengers):
                     room_points += GraftCount
                 else:
                     isAlternative = True
-                    
+
                 if "ЕСЛИ ОН ПОДХОДИТ добавляем его в список хороших вариантов" == 1:
                     good_list.append((room_points, room))
                 else:
                     alternative_list.append((room_points, room))
 
             # Тут сортируется по от меньшего кол-ва баллов до большего, тут уже всё готово
-            return {"alternative_list": list(map(lambda x: x[1].to_json(), sorted(alternative_list, key=lambda x: x[0]))),
-                    "good_list": list(map(lambda x: x[1].to_json(), sorted(good_list, key=lambda x: x[0])))}
+            return {
+                "alternative_list": list(map(lambda x: x[1].to_json(), sorted(alternative_list, key=lambda x: x[0]))),
+                "good_list": list(map(lambda x: x[1].to_json(), sorted(good_list, key=lambda x: x[0])))}
         elif request_type.isdigit():
             data = TableRoom.query.get(int(request_type))
             if data:
@@ -105,5 +104,6 @@ class Room(Resource):
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
+
 def pointsForAge(x):
-    return (1.5 * sigmoid(5/x)) - 1.05
+    return (1.5 * sigmoid(5 / x)) - 1.05
