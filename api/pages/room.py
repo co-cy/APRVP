3d8b0c2af8e02dab862f8d3190fd4e14a3cb5100
karplyukksenia <karplyukksenia@yandex.ru>
2021-09-18 10:53:18 +1000

    add counting of passenger_points

--- api/pages/room.py
+++ api/pages/room.py
@@ -1,6 +1,6 @@
 from api.database.tables.room import Room as TableRoom
 from flask_restful import Resource, reqparse
-
+import Math
 
 class Room(Resource):
     def get(self, request_type: str):
@@ -23,12 +23,16 @@ class Room(Resource):
             # neighbors ЧТО 100 % должно быть в комнате
             parser.add_argument('neighborsAge', type=list, required=True)
             parser.add_argument('neighborsHasPet', type=bool, required=True)
+            parser.add_argument('neighborsSmoking', type=bool, required=True)
+            parser.add_argument('neighborsHasChild', type=bool, required=True)
+            
 
             # ОТСЮДа брать данные о пользователе по типу all_user_parameters["gender"] или all_user_parameters["age"]
             all_user_parameters = parser.parse_args()
 
             alternative_list = []
             good_list = []
+
 
             # ЗАДАЧА: Посчитать сколько баллов получает комната
             for room in TableRoom.query.filter_by(is_free=True).all():
@@ -40,10 +44,43 @@ class Room(Resource):
                     break
 
                 # сумма баллов этой комноты
+                isAlternative = False
+                GraftCount = 0
                 room_points = 0
                 for passenger in all_passengers: 
                     passenger_points = 0
+                    if passenger.hasPet != all_user_parameters["neighborsHasPet"]:
+                        isAlternative = True
+                        break
+                    if passenger.smoking != all_user_parameters["neighborsSmoking"]:
+                        isAlternative = True
+                        break
+                    if passenger.hasChild != all_user_parameters["neighborsHasChild"]:
+                        isAlternative = True
+                        break
+                    if passenger.hasGraft:
+                        GraftCount += 1
+                    if passenger.communication == all_user_parameters["desire_communicate"]:
+                        passenger_points += 1
+                    else:
+                        passenger_points -= 1
+                    if passenger.age in all_user_parameters["neighborsAge"]:
+                        passenger_points += 2
+                    else:
+                        max_neighborsAge = max(all_user_parameters["neighborsAge"])
+                        min_neighborsAge = min(all_user_parameters["neighborsAge"])
+                        if passenger.age > max_neighborsAge:
+                            difference = passenger.age - max_neighborsAge
+                        elif passenger.age < min_neighborsAge:
+                            difference = min_neighborsAge - passenger.age
+                        passenger_points += pointsForAge(difference)
+
                     room_points += passenger_points/max_count_passenger
+                
+                if GraftCount >= len(all_passengers):
+                    room_points += GraftCount
+                else:
+                    isAlternative = True
                     
                 if "ЕСЛИ ОН ПОДХОДИТ добавляем его в список хороших вариантов" == 1:
                     good_list.append((room_points, room))
@@ -61,3 +98,10 @@ class Room(Resource):
                 return {"message": "id room not in database"}, 400
         else:
             return {"message": "Bad request"}, 400
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+def pointsForAge(x):
+    return (1.5 * sigmoid(5/x)) - 1.05
